6ae926f8f109727733a9ffd26f891f0c916f5745
Musale Martin <martinmshale@gmail.com>
2017-03-10 08:49:03 +0300

    :clap: Hackerrank solution for problem 004

--- 004.py
+++ 004.py
@@ -1,5 +1,6 @@
 """
-Project Euler Problem 4
+Project Euler Problem 4.
+
 =======================
 
 A palindromic number reads the same both ways. The largest palindrome made
@@ -7,5 +8,28 @@ from the product of two 2-digit numbers is 9009 = 91 * 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
+# !/bin/python
 
 
+def generate_palindromes():
+    """Generate all palindromes to nab memory timeout."""
+    palindromes = []
+    for j in xrange(100, 999):
+        for k in xrange(j, 999):
+            prod = j*k
+            if str(prod) == str(prod)[::-1]:
+                palindromes.append(prod)
+    return palindromes
+
+
+palindromes = generate_palindromes()
+
+t = int(raw_input().strip())
+for a0 in xrange(t):
+    n = int(raw_input().strip())
+    list_palindromes = []
+    for palindrome in palindromes:
+        if palindrome < n and palindrome >= 101101:
+            list_palindromes.append(palindrome)
+
+    print max(list_palindromes)
