b698f8374c2ff368bd9c34bd921c23a792a8aab2
musale <martinmshale@gmail.com>
2017-06-12 17:44:48 +0300

    Add accepted answer in hackerrank :clap:

--- 005.py
+++ 005.py
@@ -8,5 +8,16 @@ from 1 to 10 without any remainder.
 What is the smallest number that is evenly divisible by all of the numbers
 from 1 to 20?
 """
+#!/bin/python
 
+import sys
+def gcd(a,b):
+    return gcd(b,a%b) if b else a
 
+def lcm(a,b):
+    return a/gcd(a,b)*b
+
+t = int(raw_input().strip())
+for a0 in xrange(t):
+    n = int(raw_input().strip())
+    print reduce(lcm, range(1, n + 1))
